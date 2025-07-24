78d02422d760d0ae42c6d87222287007e786c182
Giacomo Guaresi <guaresi.giacomo@gmail.com>
2025-07-24 12:54:51 +0200

    fix: thumbnail and metadata

--- src/libslic3r/utils.cpp
+++ src/libslic3r/utils.cpp
@@ -1162,7 +1162,7 @@ std::string string_printf(const char *format, ...)
 
 std::string header_slic3r_generated()
 {
-	return std::string(SLIC3R_APP_NAME " " SoftFever_VERSION);
+	return std::string("OrcaSlicer " SoftFever_VERSION);
 }
 
 std::string header_gcodeviewer_generated()
